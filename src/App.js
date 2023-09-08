d33cdf505c3068abf64a1312be82f7a2cb6bc828
shashankjakhar07 <shashankjakhar007@gmail.com>
2023-09-08 15:47:07 +0530

    Final

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import React from 'react'
 import './App.css';
-import Login from './components/login/Login';
+import Login from './components/jsx/Login';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Register from './components/login/Register';
-import Homepage from './components/login/Homepage';
-import Welcome from './components/login/Welcome';
+import Register from './components/jsx/Register';
+import Welcome from './components/jsx/Welcome';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-      {/* <Route path='/' element={<Sampler/>}/>  */}
-      <Route path='/' element={<Homepage/>}/>
+      <Route path='/' element={<Login/>}/>
       <Route path="/login" element={<Login />}/>
       <Route path='/register' element={<Register/>}/>
       <Route path='/welcome' element={<Welcome/>}/>
